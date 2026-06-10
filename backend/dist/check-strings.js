dd2736a36e4c0cced659589e56dd713675c95623
Jose <josefernandonarvasantoyo@gmail.com>
2026-06-10 17:00:45 -0500

    Fix: Replace tsc with tsup to solve OOM on server

--- backend/dist/check-strings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-async function check() {
-    const users = await prisma.user.findMany();
-    console.log('|' + users.map(u => `"${u.email}"`).join(', ') + '|');
-}
-check().finally(() => prisma.$disconnect());
--- backend/dist/routes/redemption.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const redemption_controller_1 = require("../controllers/redemption.controller");
-const router = (0, express_1.Router)();
-router.post('/', redemption_controller_1.createRedemption);
-router.get('/history', redemption_controller_1.getHistory);
-router.get('/check-dni', redemption_controller_1.checkDniLimit);
-exports.default = router;
